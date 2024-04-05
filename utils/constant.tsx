31e824c490fad02c6e96ffedf7e867da506c4916
dragoon4890 <harshsks123@gmail.com>
2024-04-05 12:23:28 +0530

    hello2

--- utils/constant.tsx
+++ utils/constant.tsx
@@ -8,79 +8,61 @@ export const products = [
     {
       title: 'Moonbeam',
       link: 'https://gomoonbeam.com',
-      thumbnail: '/p1.png',
+      thumbnail: '/1.png',
     },
     {
       title: 'Cursor',
       link: 'https://cursor.so',
-      thumbnail: '/p2.png',
+      thumbnail: '/2.png',
     },
     {
       title: 'Rogue',
       link: 'https://userogue.com',
-      thumbnail: '/p3.png',
+      thumbnail: '/3.png',
     },
   
     {
       title: 'Editorially',
       link: 'https://editorially.org',
-      thumbnail: '/p4.png',
+      thumbnail: '/4.png',
     },
     {
       title: 'Editrix AI',
       link: 'https://editrix.ai',
-      thumbnail: '/p5.png',
+      thumbnail: '/5.png',
     },
     {
       title: 'Pixel Perfect',
       link: 'https://app.pixelperfect.quest',
-      thumbnail: '/p6.png',
+      thumbnail: '/6.png',
     },
   
     {
       title: 'Algochurn',
       link: 'https://algochurn.com',
-      thumbnail: '/p1.png',
+      thumbnail: '/5.webp',
     },
     {
       title: 'Aceternity UI',
       link: 'https://ui.aceternity.com',
-      thumbnail: '/p2.png',
+      thumbnail: '/7.avif',
     },
     {
       title: 'Tailwind Master Kit',
       link: 'https://tailwindmasterkit.com',
-      thumbnail: '/p3.png',
+      thumbnail: '/jordan-stadium-90-older-shoes-2J5HrK.jpg',
     },
     {
       title: 'SmartBridge',
       link: 'https://smartbridgetech.com',
-      thumbnail: '/p4.png',
+      thumbnail: '/jumpman-mvp-shoes-JV1HCs.png',
     },
     {
       title: 'Renderwork Studio',
       link: 'https://renderwork.studio',
-      thumbnail: '/p5.png',
+      thumbnail: '/jumpman-mvp-shoes-JV1HCs.png',
     },
   
-    {
-      title: 'Creme Digital',
-      link: 'https://cremedigital.com',
-      thumbnail: '/p6.png',
-    },
-    {
-      title: 'Golden Bells Academy',
-      link: 'https://goldenbellsacademy.com',
-      thumbnail: '/p1.png',
-    },
-    {
-      title: 'Invoker Labs',
-      link: 'https://invoker.lol',
-      thumbnail: '/p2.png',
-    },
-    {
-      title: 'E Free Invoice',
-      link: 'https://efreeinvoice.com',
-      thumbnail: '/p3.png',
-    },
+    
+ 
   ]
